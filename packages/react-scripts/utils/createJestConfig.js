f4fdea20adb7efa7c9cf465cc41a98275ad8c1f1
Vadzim <v@vadzim.info>
2016-10-28 15:51:56 +0300

    Fixes https://github.com/facebookincubator/create-react-app/issues/952 (#953)

--- packages/react-scripts/utils/createJestConfig.js
+++ packages/react-scripts/utils/createJestConfig.js
@@ -24,7 +24,7 @@ module.exports = (resolve, rootDir, isEjecting) => {
     moduleDirectories: [paths.appSrc, paths.appNodeModules, paths.ownNodeModules],
     moduleFileExtensions: ['jsx', 'js', 'json'],
     moduleNameMapper: {
-      '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': resolve('config/jest/FileStub.js'),
+      '^.+\\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': resolve('config/jest/FileStub.js'),
       '^.+\\.(css|scss)$': resolve('config/jest/CSSStub.js')
     },
     setupFiles: [resolve('config/polyfills.js')],
