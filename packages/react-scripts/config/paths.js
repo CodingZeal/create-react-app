cbb99c0aac4e08bac270ed5e8d1a5c96b7f542bb
Fabrizio Castellarin <f.castellarin@gmail.com>
2016-11-20 19:34:18 +0100

    Move the remove-on-eject-end tag at the end of the file (#978)

--- packages/react-scripts/config/paths.js
+++ packages/react-scripts/config/paths.js
@@ -73,7 +73,6 @@ module.exports = {
   ownNodeModules: resolveOwn('../node_modules'),
   nodePaths: nodePaths
 };
-// @remove-on-eject-end
 
 // config before publish: we're in ./packages/react-scripts/config/
 if (__dirname.indexOf(path.join('packages', 'react-scripts', 'config')) !== -1) {
@@ -91,3 +90,4 @@ if (__dirname.indexOf(path.join('packages', 'react-scripts', 'config')) !== -1) 
     nodePaths: nodePaths
   };
 }
+// @remove-on-eject-end
