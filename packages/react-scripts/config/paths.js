e18e4ff9a156878d0159ec1ef61b348b68da3e86
Dan Abramov <dan.abramov@gmail.com>
2017-03-05 23:23:51 +0000

    Fix ejecting from a scoped fork (#1727)
    * Read script names from own bin instead of guessing
    
    This fixes ejecting from a fork that uses a different bin script name.
    
    * Fix ejecting for a scoped react-scripts fork
    
    We shouldn't hardcode react-scripts because fork name might differ.
    We also shouldn't rely on it being an immediate child because scoped packages are a level deeper.
    
    * Clarify that own* properties only exist before ejecting

--- packages/react-scripts/config/paths.js
+++ packages/react-scripts/config/paths.js
@@ -86,7 +86,6 @@ module.exports = {
   yarnLockFile: resolveApp('yarn.lock'),
   testsSetup: resolveApp('client/setupTests.js'),
   appNodeModules: resolveApp('node_modules'),
-  ownNodeModules: resolveApp('node_modules'),
   nodePaths: nodePaths,
   publicUrl: getPublicUrl(resolveApp('package.json')),
   servedPath: getServedPath(resolveApp('package.json'))
@@ -100,7 +99,6 @@ function resolveOwn(relativePath) {
 // config before eject: we're in ./node_modules/react-scripts/config/
 module.exports = {
   appPath: resolveApp('.'),
-  ownPath: resolveApp('node_modules/react-scripts'),
   appBuild: resolveApp(buildPath),
   appPublic: resolveApp('client/public'),
   appHtml: resolveApp('client/public/index.html'),
@@ -110,11 +108,12 @@ module.exports = {
   yarnLockFile: resolveApp('yarn.lock'),
   testsSetup: resolveApp('client/setupTests.js'),
   appNodeModules: resolveApp('node_modules'),
-  // this is empty with npm3 but node resolution searches higher anyway:
-  ownNodeModules: resolveOwn('node_modules'),
   nodePaths: nodePaths,
   publicUrl: getPublicUrl(resolveApp('package.json')),
-  servedPath: getServedPath(resolveApp('package.json'))
+  servedPath: getServedPath(resolveApp('package.json')),
+  // These properties only exist before ejecting:
+  ownPath: resolveOwn('.'),
+  ownNodeModules: resolveOwn('node_modules'), // This is empty on npm 3
 };
 
 var reactScriptsPath = path.resolve('node_modules/react-scripts');
@@ -124,7 +123,6 @@ var reactScriptsLinked = fs.existsSync(reactScriptsPath) && fs.lstatSync(reactSc
 if (!reactScriptsLinked && __dirname.indexOf(path.join('packages', 'react-scripts', 'config')) !== -1) {
   module.exports = {
     appPath: resolveApp('.'),
-    ownPath: resolveOwn('.'),
     appBuild: resolveOwn('../../' + buildPath),
     appPublic: resolveOwn('template/client/public'),
     appHtml: resolveOwn('template/client/public/index.html'),
@@ -134,10 +132,12 @@ if (!reactScriptsLinked && __dirname.indexOf(path.join('packages', 'react-script
     yarnLockFile: resolveOwn('template/yarn.lock'),
     testsSetup: resolveOwn('template/client/setupTests.js'),
     appNodeModules: resolveOwn('node_modules'),
-    ownNodeModules: resolveOwn('node_modules'),
     nodePaths: nodePaths,
     publicUrl: getPublicUrl(resolveOwn('package.json')),
-    servedPath: getServedPath(resolveOwn('package.json'))
+    servedPath: getServedPath(resolveOwn('package.json')),
+    // These properties only exist before ejecting:
+    ownPath: resolveOwn('.'),
+    ownNodeModules: resolveOwn('node_modules'),
   };
 }
 // @remove-on-eject-end
