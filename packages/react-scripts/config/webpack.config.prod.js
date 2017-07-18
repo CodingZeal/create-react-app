964b00ba614188933b8152d1aa321f1ad12a34b0
Luke Barbuto <luke.barbuto@gmail.com>
2017-07-18 13:40:43 -1000

    Add graphql loader to production config

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -174,11 +174,12 @@ module.exports = {
 
       // "file" loader makes sure those assets end up in the `build` folder.
       // When you `import` an asset, you get its filename.
-      // ZEAL: Add .scss because we add the sass-loader below.
+      // ZEAL: Add .scss and .graphql because we add the loaders below
       {
         exclude: [
           /\.html$/,
           /\.(js|jsx)$/,
+          /\.(graphql|gql)$/,
           /\.css$/,
           /\.scss$/,
           /\.json$/,
@@ -317,6 +318,12 @@ module.exports = {
           )
         ),
         // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
+      },
+      // ZEAL: Adds support for parsing GraphQL files
+      {
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/,
+        loader: 'graphql-tag/loader',
       },
       // ** STOP ** Are you adding a new loader?
       // Remember to add the new extension(s) to the "file" loader exclusion list.
